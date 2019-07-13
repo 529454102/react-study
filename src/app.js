5451e2b82adf4898e4b6e7494085e09ee720234b
Summoner_JY <529454102@qq.com>
2019-07-13 18:04:47 +0800

    init

---
+++ src/app.js
@@ -0,0 +1,7 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import './index.sass'
+
+ReactDOM.render(<h1>hello</h1>, document.getElementById('app'))
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                  fallback: 'style-loader',
+                  use: ['css-loader', 'sass-loader']
+                })
+              }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+                template: './src/index.html'
+            }),
+        new ExtractTextPlugin({
+            filename: 'index.css'
+        })
+    ]
+};
